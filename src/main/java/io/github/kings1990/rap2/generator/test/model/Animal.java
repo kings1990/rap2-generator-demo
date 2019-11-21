8b13180696520d7da0276fc1bf0803e9fbd16999
kings <963987632@qq.com>
2019-11-21 22:34:52 +0800

    1.0.6测试

---
+++ src/main/java/io/github/kings1990/rap2/generator/test/model/Animal.java
@@ -0,0 +1,20 @@
+package io.github.kings1990.rap2.generator.test.model;
+
+import lombok.Data;
+
+@Data
+public class Animal {
+
+    /**
+     * 科目
+     */
+    private String family;
+    /**
+     * 名称
+     */
+    private String name;
+    
+    
+}
+
+

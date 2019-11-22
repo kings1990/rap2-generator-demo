38e5a0d144a5dd5149935029bd997b3f1c604489
kings <963987632@qq.com>
2019-11-22 11:25:20 +0800

    1.0.6测试

--- src/main/java/io/github/kings1990/rap2/generator/test/model/AnnoPropertyUnMatch.java
+++ src/main/java/io/github/kings1990/rap2/generator/test/model/AnnoPropertyUnMatch.java
@@ -4,19 +4,31 @@ import lombok.Data;
 
 @Data
 public class AnnoPropertyUnMatch {
-    /**
-     * id
-     */
+    
     private String id;
     
     private String name;
 
+    //aaa
     /**
      * 性别
      */
     public int sex;
+
     
+    private String id1;
+    
+    //type
     public int type;
+
+    //****#########
+    /**
+     * 性别1
+     */
+    public int sex1;
+
+    
+    public int type1;
     
 }
 
